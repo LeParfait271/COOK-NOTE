eb58cd35b88d9cb7eb7e858c5a109494cd55772d
LeParfait271 <maguire.d91@gmail.com>
2026-08-03 02:12:55 +0200

    style: refine fixed recipe command rail

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ const FALLBACK_ART_ASSETS = Object.freeze({
   appIcon: '/assets/brand/app-icon.png'
 });
 const THEME_RECIPE_ART_IMAGES = window.COOK_NOTE_THEME_RECIPE_ART || Object.freeze({ dark: Object.freeze({}), light: Object.freeze({}) });
-const SITE_VERSION = 'v4.40';
+const SITE_VERSION = 'v4.41';
 const SITE_UPDATED_AT = '03/08/26';
 const APP_RAW_DOWNLOAD_BASE = 'https://raw.githubusercontent.com/LeParfait271/COOK-NOTE/main/downloads';
 const ANDROID_LEGACY_APK_VERSION = '4.08';
